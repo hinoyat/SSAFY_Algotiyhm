6166f34675dee87798f08de9d1f771e94caf1bac
Hinoyat <hhwj2270@gmail.com>
2024-09-01 22:13:08 +0900

    240901 2000

--- for test/240831.py
+++ for test/240831.py
@@ -15,6 +15,7 @@
 #     v = 1
 #     order(idx,v)
 #     print(f'#{tc} {tree[1]} {tree[N//2]}')
+from idlelib.debugger_r import restart_subprocess_debugger
 
 
 # # 써보트링
@@ -136,8 +137,89 @@
 
     # print(f'#{tc} {result}')
     
-    
-    
-# 사칙한 연산
-T = int(input())
-for tc in range(1, T+1):
+# def supernova(num):
+#     result = ''
+#     i = 1
+#     while num != 0:
+#         if num - (2**(-i)) >= 0:
+#             result += '1'
+#             num -= (2**(-i))
+#         else:
+#             result += '0'
+#         i += 1
+#         if len(result) >= 13:
+#             return 'overflow'
+#     return result
+#
+# # 사칙한 연산
+# T = int(input())
+# for tc in range(1, T+1):
+#     N = float(input())
+#     print(f'#{tc} {supernova(N)}')
+
+# def supernova(num):
+#     if num == 1:
+#         return '1'
+#     elif num == 0:
+#        return '0'
+#
+#     if num % 2 == 0:
+#         return supernova(num//2) + '0'
+#     else:
+#         return supernova(num//2) + '1'
+#
+#
+#
+# T = int(input())
+# for tc in range(1, T+1):
+#     N, M = map(int,input().split())
+#     result = list(supernova(M))
+#     result.reverse()
+#
+#     if result[:N].count('1') == N :
+#         print(f'#{tc} ON')
+#     else :
+#         print(f'#{tc} OFF')
+
+
+
+# def change2(lst):
+#     r = 0
+#     for i in range(len(lst) - 1 , -1, -1):
+#         r += (2**((len(lst) - i - 1))*lst[i])
+#     return r
+#
+# def change3(lst):
+#     r = 0
+#     for i in range(len(lst) - 1 , -1, -1):
+#         r += (3**((len(lst) - i - 1))*lst[i])
+#     return r
+#
+#
+#
+# T = int(input())
+# for tc in range(1, T+1):
+#     ez = list(map(int, input()))
+#     sz = list(map(int, input()))
+#
+#     result = 0
+#     for i in range(len(ez)):
+#         ez[i] = (1 + ez[i]) % 2
+#         e = change2(ez)
+#         for j in range(len(sz)):
+#             for _ in range(2):
+#                 sz[j] = (1 + sz[j]) % 3
+#                 s = change3(sz)
+#
+#                 if s == e:
+#                     result = e
+#                     break
+#             sz[j] = (1 + sz[j]) %3
+#             if result != 0:
+#                 break
+#         if result != 0:
+#             break
+#
+#
+#         ez[i] = (1 + ez[i]) % 2
+#     print(f'#{tc} {result}')
